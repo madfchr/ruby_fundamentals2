7e2416e860922caf8aac057e92a8482489883475
Ariane Deschenes Foucher <ariane.df@me.com>
2016-03-23 15:45:54 -0400

    steps 2 and 3 completed, but still working on step 1

--- exercise6.rb
+++ exercise6.rb
@@ -1,2 +1,18 @@
+# STEP 1
 grocery_list = ["avocados", "kiwis", "bananas", "quinoa"]
 grocery_list.each { |item| puts "* #{item}" }
+
+# STEP 1.2 ?
+grocery_list << "rice"
+puts grocery_list.each { |item| puts "* #{item}" }
+
+# STEP 2
+puts "There's #{grocery_list.count } items on the list!"
+
+# STEP 3
+
+if grocery_list.include?("bananas")
+  puts "You don't need to pick up bananas today."
+else
+  puts "You need to pick up bananas."
+end
