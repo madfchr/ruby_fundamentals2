e64d99c8ef0f1b513980460427ff88b812b116c8
Ariane Deschenes Foucher <ariane.df@me.com>
2016-03-23 17:19:13 -0400

    all steps completed

--- exercise6.rb
+++ exercise6.rb
@@ -1,18 +1,35 @@
 # STEP 1
+def asterisk(grocery_list)
+  grocery_list.each do |item|
+    puts "* #{item}"
+  end
+end
+
 grocery_list = ["avocados", "kiwis", "bananas", "quinoa"]
-grocery_list.each { |item| puts "* #{item}" }
+asterisk(grocery_list)
 
 # STEP 1.2 ?
 grocery_list << "rice"
-puts grocery_list.each { |item| puts "* #{item}" }
+asterisk(grocery_list)
 
 # STEP 2
 puts "There's #{grocery_list.count } items on the list!"
 
 # STEP 3
-
 if grocery_list.include?("bananas")
   puts "You don't need to pick up bananas today."
 else
   puts "You need to pick up bananas."
 end
+
+# STEP 4
+puts "The second item on the list is: #{grocery_list[1]}!"
+
+# STEP 5
+#puts grocery_list.sort
+asterisk(grocery_list.sort)
+
+# STEP 6
+# I'm deleting quinoa instead of salmon because I'm allergic to fish.
+grocery_list.delete_if {|item| item == "quinoa"}
+asterisk(grocery_list)
